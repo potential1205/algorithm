68f666ec77fae086b0f9d15ebd43b180845f5857
이재훈 <potential1205@naver.com>
2024-10-10 20:26:30 +0900

    20241010

--- src/algorithm_study/week12/b1194_달이_차오른다_가자/Main.java
+++ src/algorithm_study/week12/b1194_달이_차오른다_가자/Main.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.Map;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -18,7 +17,6 @@ import java.util.StringTokenizer;
 */
 
 public class Main {
-
     static char[][] board;
     static int N,M,answer;
     static Point start;
@@ -26,17 +24,7 @@ public class Main {
     static int[] dx = {0,1,0,-1};
 
     static class Point{
-        int y,x,cnt,keyNum;
-
-        @Override
-        public String toString() {
-            return "Point{" +
-                    "y=" + y +
-                    ", x=" + x +
-                    ", cnt=" + cnt +
-                    ", keyNum=" + keyNum +
-                    '}';
-        }
+        int y, x, cnt, keyNum;
 
         public Point(int y, int x, int cnt, int keyNum) {
             this.y = y;
@@ -52,7 +40,7 @@ public class Main {
 
         boolean[][][] visit = new boolean[N][M][64];
         visit[start.y][start.x][0] = true;
-        board[start.y][start.x]='.';
+        board[start.y][start.x] = '.';
 
         while(!queue.isEmpty()){
             Point cur = queue.poll();
@@ -65,31 +53,33 @@ public class Main {
                 int ny = cur.y + dy[i];
                 int nx = cur.x + dx[i];
 
-                if(ny<0 || nx<0 || ny>=N || nx>=M || visit[ny][nx][cur.keyNum] || board[ny][nx]=='#') continue;
+                if(ny<0 || nx<0 || ny>=N || nx>=M || visit[ny][nx][cur.keyNum] || board[ny][nx]=='#') {
+                    continue;
+                }
 
-                if(65<=board[ny][nx] && board[ny][nx]<=70){
-                    int state = cur.keyNum;
-                    int keyIndex = 1<< (5-((int) board[ny][nx]-97));
-                    if((state&keyIndex)!=0){
+                if('A' <= board[ny][nx] && board[ny][nx] <= 'F'){
+                    int keyIndex = 1 << (5-((int) board[ny][nx]-97));
+
+                    if((cur.keyNum & keyIndex) != 0){
                         queue.offer(new Point(ny, nx, cur.cnt+1, cur.keyNum));
                         visit[ny][nx][cur.keyNum] = true;
                     }
-                } else if(97<=board[ny][nx] && board[ny][nx]<=102){
-                    // 해당 키를 가지고 있지 않은 경우에만 추가해주기
-                    int state = cur.keyNum;
-                    int keyIndex = 1<< (5-((int) board[ny][nx]-97));
 
-                    if((state&keyIndex)==0){
+                } else if('a' <= board[ny][nx] && board[ny][nx] <= 'f'){
+                    int keyIndex = 1 << (5-((int) board[ny][nx]-97));
+
+                    if((cur.keyNum & keyIndex) == 0){
                         int num = (int) Math.pow(2, 5-((int) board[ny][nx] - 97));
 
                         queue.offer(new Point(ny, nx, cur.cnt+1, cur.keyNum + num));
                         visit[ny][nx][cur.keyNum + num] = true;
+
                     } else{
                         queue.offer(new Point(ny, nx, cur.cnt+1, cur.keyNum ));
                         visit[ny][nx][cur.keyNum] = true;
                     }
 
-                } else if(board[ny][nx]=='.' || board[ny][nx]=='1'){
+                } else if(board[ny][nx] == '.' || board[ny][nx] == '1'){
                     queue.offer(new Point(ny, nx, cur.cnt+1, cur.keyNum));
                     visit[ny][nx][cur.keyNum] = true;
                 }
@@ -118,7 +108,9 @@ public class Main {
             for(int j=0; j<M; j++){
                 board[i][j] = line.charAt(j);
 
-                if(board[i][j]=='0') start = new Point(i,j,0,0);
+                if(board[i][j]=='0') {
+                    start = new Point(i,j,0,0);
+                }
             }
         }
 
