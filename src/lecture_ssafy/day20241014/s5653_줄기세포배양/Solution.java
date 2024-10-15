b7479348b9638bdbfb433f1655f26420fda304e4
이재훈 <potential1205@naver.com>
2024-10-15 20:31:18 +0900

    20241015

--- src/lecture_ssafy/day20241014/s5653_줄기세포배양/Solution.java
+++ src/lecture_ssafy/day20241014/s5653_줄기세포배양/Solution.java
@@ -8,32 +8,77 @@ import java.util.*;
 public class Solution {
     static int T, N, M, K, answer;
     static List<Cell> cellList;
+    static boolean[][] visit;
     static int[] dy = {-1,0,1,0};
     static int[] dx = {0,1,0,-1};
 
-    static class Cell {
-        int y, x, hp, state;
+    enum CellState {
+        INIT, ACTIVE, DEAD;
+    }
 
-        Cell(int y, int x, int t, int hp, int state) {
+    static class Cell implements Comparable<Cell>{
+        int y, x, hp, changeTime;
+        CellState cellState;
+
+        Cell(int y, int x, int hp, int changeTime, CellState cellState) {
             this.y = y;
             this.x = x;
             this.hp = hp;
-            this.state = state; // -1 사망, 0 비활성, 1 활성
+            this.cellState = cellState;
+            this.changeTime = changeTime;
         }
-    }
 
-    static void updateCellState() {
-
+        @Override
+        public int compareTo(Cell o) {
+            return o.hp - this.hp;
+        }
     }
 
     static void solution(int tc) {
         answer = 0;
-        for (int time = 1; time <= K; time++) {
-            // 세포 상태 최산화
-            updateCellState();
 
-            //
-            cellCulture(time);
+        PriorityQueue<Cell> pq = new PriorityQueue<>();
+
+        for (int time = 1; time <= K; time++) {
+            while (!pq.isEmpty()) {
+                Cell cell = pq.poll();
+
+                if (visit[cell.y][cell.x]) continue;
+
+                cellList.add(cell);
+                visit[cell.y][cell.x] = true;
+            }
+
+            for (int i = 0; i < cellList.size(); i++) {
+                Cell cell = cellList.get(i);
+
+                if (cell.cellState.equals(CellState.DEAD)) continue;
+
+                // 활성 상태로 전환 후 사방으로 세포 번식
+                if (cell.cellState.equals(CellState.INIT) && cell.changeTime == time) {
+                    cell.cellState = CellState.ACTIVE;
+                    cell.changeTime = cell.changeTime + cell.hp;
+
+                    for (int d = 0; d < 4; d++) {
+                        int ny = cell.y + dy[d];
+                        int nx = cell.x + dx[d];
+
+                        pq.offer(new Cell(ny, nx, cell.hp, cell.changeTime+1, CellState.INIT));
+                    }
+
+                } else if (cell.cellState.equals(CellState.ACTIVE) && cell.changeTime == time) {
+                    cell.cellState = CellState.DEAD;
+                }
+            }
+        }
+
+        for (int i = 0; i < cellList.size(); i++) {
+            Cell cell = cellList.get(i);
+
+            if (cell.cellState.equals(CellState.INIT)
+                    || cell.cellState.equals(CellState.ACTIVE)) {
+                answer++;
+            }
         }
 
         System.out.println("#" + tc + " " + answer);
@@ -51,6 +96,7 @@ public class Solution {
             M = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
 
+            visit = new boolean[N+2*K][M+2*K];
             cellList = new ArrayList<>();
 
             for (int i = 0; i < N; i++) {
@@ -58,7 +104,11 @@ public class Solution {
 
                 for (int j = 0; j < M; j++) {
                     int hp = Integer.parseInt(st.nextToken());
-                    cellList.add(new Cell(i, j, 0, hp, 0));
+
+                    if (hp != 0) {
+                        cellList.add(new Cell(K+i, K+j, hp, hp, CellState.INIT));
+                        visit[K+i][K+j] = true;
+                    }
                 }
             }
 
@@ -66,3 +116,4 @@ public class Solution {
         }
     }
 }
+
