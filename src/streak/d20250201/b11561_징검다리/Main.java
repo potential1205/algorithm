a2b9abd86985d79e0edba03d64086cd8b8f6549a
이재훈 <potential1205@naver.com>
2025-02-01 22:50:27 +0900

    2025-02-01

---
+++ src/streak/d20250201/b11561_징검다리/Main.java
@@ -0,0 +1,43 @@
+package streak.d20250201.b11561_징검다리;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int T = Integer.parseInt(st.nextToken());
+
+        for (int  tc = 0; tc < T; tc++) {
+            st = new StringTokenizer(bf.readLine());
+
+            long n = Long.parseLong(st.nextToken());
+
+            long left = 0;
+            long right = (long) Math.sqrt((2*n-1));;
+            long answer = 0;
+            while (left <= right) {
+                long mid = left + (right - left) / 2;
+                long sum = mid * (mid+1) / 2;
+
+                if (sum > n) {
+                    right = mid-1;
+                } else {
+                    left = mid+1;
+                    answer = Math.max(answer, mid);
+                }
+            }
+
+            if (n==1) {
+                System.out.println(1);
+            } else {
+                System.out.println(answer);
+            }
+        }
+    }
+}
