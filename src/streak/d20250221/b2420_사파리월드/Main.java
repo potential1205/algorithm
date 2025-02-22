fa682489e737db5e127252ee745955bde2dca32c
이재훈 <potential1205@naver.com>
2025-02-23 00:01:40 +0900

    2025-02-23

--- src/streak/d20250221/b2420_사파리월드/Main.java
+++ src/streak/d20250221/b2420_사파리월드/Main.java
@@ -1,15 +1,23 @@
 package streak.d20250221.b2420_사파리월드;
 
+import java.math.BigInteger;
 import java.util.*;
 
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        long n = sc.nextInt();
-        long m = sc.nextInt();
-        long result = n-m;
+        //long n = sc.nextInt();
+        //long m = sc.nextInt();
+        //long result = n-m;
 
-        System.out.println(Math.abs(result));
+        //System.out.println(Math.abs(result));
+
+        BigInteger val1 = new BigInteger(String.valueOf(sc.nextLong()));
+        BigInteger val2 = new BigInteger(String.valueOf(sc.nextLong()));
+
+        System.out.println(val1.subtract(val2).abs());
+
+
     }
 }
