524e76b16a36f14c02bfdc37e5418e27721bc487
이재훈 <potential1205@naver.com>
2025-03-08 22:43:28 +0900

    2025-03-08

---
+++ src/streak/d20250308/b5639_이진탐색트리/Main.java
@@ -0,0 +1,57 @@
+package streak.d20250308.b5639_이진탐색트리;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static class Node {
+        int num;
+        Node left, right;
+
+        public Node(int num) {
+            this.num = num;
+        }
+
+        public void addNode(Node newNode) {
+            if (this.num > newNode.num) {
+                if (this.left == null) {
+                    this.left = newNode;
+                } else {
+                    this.left.addNode(newNode);
+                }
+            } else {
+                if (this.right == null) {
+                    this.right = newNode;
+                } else {
+                    this.right.addNode(newNode);
+                }
+            }
+        }
+    }
+
+    static void postOrder(Node cur) {
+        if(cur == null) return;
+
+        postOrder(cur.left);
+        postOrder(cur.right);
+        System.out.println(cur.num);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        Node root = new Node(Integer.parseInt(bf.readLine()));
+
+        while(true) {
+            String temp = bf.readLine();
+
+            if (temp == null || temp.equals("")) break;
+
+            root.addNode(new Node(Integer.parseInt(temp)));
+        }
+
+        postOrder(root);
+    }
+}
