792d91a40b9efc7cb6f48047fdc28672157229cc
이재훈 <potential1205@naver.com>
2025-03-14 23:22:43 +0900

    2025-03-14

---
+++ src/streak/d20250314/b7432_티스크_트리/Main.java
@@ -0,0 +1,55 @@
+package streak.d20250314.b7432_티스크_트리;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+public class Main {
+
+    static int n;
+    static Node root;
+
+    static class Node {
+        Map<String, Node> child = new TreeMap<>();
+    }
+
+    static void print(Node cur, int depth) {
+        for (String path : cur.child.keySet()) {
+
+            for (int i = 0; i < depth; i++) {
+                System.out.print(" ");
+            }
+
+            System.out.print(path);
+            System.out.println();
+
+            print(cur.child.get(path), depth+1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+
+        root = new Node();
+
+        for (int i = 0; i < n; i++) {
+            String[] paths = br.readLine().split("\\\\");
+
+            Node cur = root;
+
+            for (String path : paths) {
+                cur.child.putIfAbsent(path, new Node());
+                cur = cur.child.get(path);
+            }
+        }
+
+        print(root, 0);
+    }
+}
