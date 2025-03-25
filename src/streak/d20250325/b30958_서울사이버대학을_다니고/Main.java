5e967ca13193077f4ae9ddd0a4c38c74ba694fbb
이재훈 <potential1205@naver.com>
2025-03-25 23:18:44 +0900

    2025-03-25

---
+++ src/streak/d20250325/b30958_서울사이버대학을_다니고/Main.java
@@ -0,0 +1,37 @@
+package streak.d20250325.b30958_서울사이버대학을_다니고;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int answer = 0;
+
+        int n = sc.nextInt();
+        String blank = sc.nextLine();
+        String str = sc.nextLine();
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char ch : str.toCharArray()) {
+            if ('a' <= ch && ch <= 'z') {
+                if (map.containsKey(ch)) {
+                    int cnt = map.get(ch) + 1;
+                    map.put(ch, cnt);
+                    answer = Math.max(answer ,cnt);
+                } else {
+                    int cnt = 1;
+                    map.put(ch, cnt);
+                    answer = Math.max(answer ,cnt);
+                }
+            }
+
+        }
+
+        System.out.println(answer);
+    }
+}
