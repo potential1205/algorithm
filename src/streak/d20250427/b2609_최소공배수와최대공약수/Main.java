74a234d0bee4cdb8ca006c10879923b447a4f1fb
이재훈 <potential1205@naver.com>
2025-04-27 23:18:42 +0900

    2025-04-27

---
+++ src/streak/d20250427/b2609_최소공배수와최대공약수/Main.java
@@ -0,0 +1,27 @@
+package streak.d20250427.b2609_최소공배수와최대공약수;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int num1 = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
+
+        int maxNum = gcd(num1, num2);
+
+        System.out.println(maxNum);
+        System.out.println(num1 * num2 / maxNum);
+    }
+
+    public static int gcd(int num1, int num2) {
+        if (num2 == 0) {
+            return num1;
+        }
+
+        return gcd(num2, num1 % num2);
+    }
+}
