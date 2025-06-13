4b3573d0864686539b308a6af11181948ab6ecea
이재훈 <potential1205@naver.com>
2025-06-13 20:29:24 +0900

    2025-06-13

---
+++ src/streak/d20250430/b1110_더하기_사이클/Main.java
@@ -0,0 +1,29 @@
+package streak.d20250430.b1110_더하기_사이클;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int value = n;
+        int cnt = 0;
+
+        while (true) {
+            int a = value / 10;
+            int b = value % 10;
+
+            value = b * 10 + (a + b) % 10;
+            cnt++;
+
+            if (n == value) {
+                break;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
