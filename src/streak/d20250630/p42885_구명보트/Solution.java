0735304cf0725524e2cca27415cb13d96c1f9c19
이재훈 <potential1205@naver.com>
2025-06-30 12:12:08 +0900

    2025-06-30

---
+++ src/streak/d20250630/p42885_구명보트/Solution.java
@@ -0,0 +1,27 @@
+package streak.d20250630.p42885_구명보트;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int n = people.length;
+        int x1 = 0;
+        int x2 = n - 1;
+
+        Arrays.sort(people);
+
+        while (x1 <= x2) {
+            if (people[x1] + people[x2] <= limit) {
+                answer++;
+                x1++;
+                x2--;
+            } else {
+                answer++;
+                x2--;
+            }
+        }
+
+        return answer;
+    }
+}
