ddf3a43d2b7dff3d6a047b93283c9b49dc740d42
이재훈 <potential1205@naver.com>
2025-07-04 13:36:28 +0900

    2025-07-04

---
+++ src/streak/d20250704/p42898_등굣길/Solution.java
@@ -0,0 +1,43 @@
+package streak.d20250704.p42898_등굣길;
+
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        int[][] dp = new int[n][m];
+
+        for (int i = 0; i < puddles.length; i++) {
+            dp[puddles[i][1] - 1][puddles[i][0] - 1] = -1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (dp[i][0] != -1) {
+                dp[i][0] = 1;
+            } else {
+                break;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            if (dp[0][i] != -1) {
+                dp[0][i] = 1;
+            } else {
+                break;
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (dp[i][j] == -1) continue;
+
+                if (dp[i - 1][j] != -1) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j]) % (1000000007);
+                }
+
+                if (dp[i][j - 1] != -1) {
+                    dp[i][j] = (dp[i][j] + dp[i][j - 1]) % (1000000007);
+                }
+            }
+        }
+
+        return dp[n-1][m-1] % (1000000007);
+    }
+}
