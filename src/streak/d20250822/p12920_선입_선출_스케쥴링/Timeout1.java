3b00b27cf772cd11cc64c59988ec51606f3760f1
이재훈 <potential1205@naver.com>
2025-08-22 12:35:50 +0900

    2025-08-22

---
+++ src/streak/d20250822/p12920_선입_선출_스케쥴링/Timeout1.java
@@ -0,0 +1,54 @@
+package streak.d20250822.p12920_선입_선출_스케쥴링;
+
+import java.util.*;
+
+class Timeout1 {
+
+    static class Node implements Comparable<Node> {
+        int id;
+        int activeTime;
+
+        Node(int id, int activeTime) {
+            this.id = id;
+            this.activeTime = activeTime;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            if (this.activeTime != o.activeTime) {
+                return this.activeTime - o.activeTime;
+            } else {
+                return this.id - o.id;
+            }
+        }
+    }
+
+    public int solution(int n, int[] cores) {
+        int answer = 0;
+
+        int[] coreState = new int[cores.length];
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        for (int i = 0; i < cores.length; i++) {
+            pq.offer(new Node(i, 0));
+        }
+
+        int time = 0;
+        while (true) {
+            if (n == 0) {
+                break;
+            }
+
+            while (n > 0 && !pq.isEmpty() && pq.peek().activeTime == time) {
+                Node node = pq.poll();
+                pq.offer(new Node(node.id, time + cores[node.id]));
+                answer = node.id + 1;
+                n--;
+            }
+
+            time++;
+        }
+
+        return answer;
+    }
+}
