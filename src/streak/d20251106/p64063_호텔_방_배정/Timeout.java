877828a013cbb3444a3c02e5ae440c3d1dcb126a
이재훈 <potential1205@naver.com>
2025-11-06 10:09:33 +0900

    2025-11-06

---
+++ src/streak/d20251106/p64063_호텔_방_배정/Timeout.java
@@ -0,0 +1,42 @@
+package streak.d20251106.p64063_호텔_방_배정;
+
+import java.util.*;
+
+class Timeout {
+
+    static Map<Long, Long> map = new HashMap<>();
+
+    static long find(long num) {
+        if (!map.containsKey(num)) {
+            return num;
+        }
+
+        return find(map.get(num));
+    }
+
+
+    public long[] solution(long k, long[] room_number) {
+
+        int n = room_number.length;
+
+        long[] answer = new long[n];
+
+        for (int i = 0; i < n; i++) {
+            long room = room_number[i];
+
+            if (!map.containsKey(room)) {
+                answer[i] = room;
+                long emptyRoom = find(room + 1);
+                map.put(room, emptyRoom);
+            } else{
+                long emptyRoom = find(room + 1);
+                answer[i] = emptyRoom;
+                long nextEmptyRoom = find(emptyRoom + 1);
+                map.put(emptyRoom, nextEmptyRoom);
+            }
+        }
+
+
+        return answer;
+    }
+}
